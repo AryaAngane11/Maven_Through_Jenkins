c7475fc693bd36ed8215109facd4141849d4bfa4
Arya A <95465577+AryaAngane11@users.noreply.github.com>
2023-09-15 15:46:06 +0530

    Add files via upload
    Java filed uploaded

---
+++ FirstTestClass.java
@@ -0,0 +1,14 @@
+package basicweb;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.letskodeit.com");
+
+	}
+
+}
